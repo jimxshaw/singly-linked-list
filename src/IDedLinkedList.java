ea5f6161b31b608d2edf75f1f69c9c7f80255e83
jimxshaw <jimxshaw@gmail.com>
2025-02-24 12:40:19 -0600

    Implement Link static inner class.

--- src/IDedLinkedList.java
+++ src/IDedLinkedList.java
@@ -1,2 +1,31 @@
 public class IDedLinkedList<T extends IDedObject> {
+    private final Link<T> sentinel;
+
+    IDedLinkedList() {
+        // The sentinel's ID should not conflict with an actual Link's ID.
+        this.sentinel = new Link<>(Integer.MIN_VALUE, null, null);
+    }
+
+    // TODO: Implement the class methods.
+//    public void makeEmpty() {}
+
+//    public T findID(int ID) {}
+
+//    public boolean insertAtFront(T type) {}
+
+//    T deleteFromFront() {}
+
+//    int printTotal() {}
+
+    private static class Link<T> {
+        int ID;
+        Link<T> next;
+        T data;
+
+        Link(int ID, Link<T> next, T data) {
+            this.ID = ID;
+            this.next = next;
+            this.data = data;
+        }
+    }
 }
